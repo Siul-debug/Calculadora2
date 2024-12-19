5d2884b517bcf6bc31c724f885ae14c155779166
Siul-debug <1923517@alu.murciaeduca.es>
2024-12-19 15:52:51 -0500

    Creado main y operadores

---
+++ Calculadora2.java
@@ -0,0 +1,8 @@
+public class Calculadora2 {
+    public static void main(String[] args) {
+    // Valores predefinidos (puedes cambiarlos aquí)
+    double num1 = 10.5;
+    char operador = '+';
+    double num2 = 3.2;
+    }
+    }
