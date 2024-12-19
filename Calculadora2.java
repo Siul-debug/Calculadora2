0685d7d11b2faadc0dc60f276b6aa53ebe6de37f
Siul-debug <1923517@alu.murciaeduca.es>
2024-12-19 15:59:16 -0500

    Añadida operación resta

--- Calculadora2.java
+++ Calculadora2.java
@@ -2,7 +2,17 @@ public class Calculadora2 {
     public static void main(String[] args) {
     // Valores predefinidos (puedes cambiarlos aquí)
     double num1 = 10.5;
-    char operador = '+';
+    char operador = '-';
     double num2 = 3.2;
+    double resultado = 0;
+    switch (operador) {
+    case '-':
+    resultado = num1 - num2;
+    break;
+    default:
+    System.out.println("Operador inválido");
+    return;
     }
+    System.out.println("Resultado: " + resultado);
     }
+}
