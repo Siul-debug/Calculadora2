606809cffe88f72e8abca4ce9504d6010ab6e7d1
Siul-debug <1923517@alu.murciaeduca.es>
2024-12-19 16:01:08 -0500

    Añadida división

--- Calculadora2.java
+++ Calculadora2.java
@@ -2,7 +2,21 @@ public class Calculadora2 {
     public static void main(String[] args) {
     // Valores predefinidos (puedes cambiarlos aquí)
     double num1 = 10.5;
-    char operador = '+';
+    char operador = '/';
     double num2 = 3.2;
+    double resultado = 0;
+    switch (operador) {
+    case '/':
+    if (num2 == 0) {
+    System.out.println("Error: División entre cero");
+    } else {
+    resultado = num1 / num2;
+    }
+    break;
+    default:
+    System.out.println("Operador inválido");
+    return;
+    }
+    System.out.println("Resultado: " + resultado);
     }
     }
